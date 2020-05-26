99c856e1a97b6af7f9354e0db62edbab16db9ce2
Aaron <aaron3993@hotmail.com>
2020-05-26 20:29:13 +0000

    Finished implementArrays

---
+++ eqArrays.js
@@ -0,0 +1,14 @@
+const assertEqual = (actual, expected) => {
+  console.log(actual === expected
+    ? `✅✅✅ Assertion Passed: ${actual} === ${expected}`
+    : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    };
+  };
+  return true;
+};
