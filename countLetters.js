d73d03b731b13dd8f2154b2f321ff2650c68d069
Aaron <aaron3993@hotmail.com>
2020-05-27 17:53:17 +0000

    Created countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = word => {
+  const output = {}
+  for (let letter of word.split(' ').join('')) {
+    if (output[letter]) {
+      output[letter]++
+    } else {
+      output[letter] = 1
+    }
+  }
+  return output
+}
